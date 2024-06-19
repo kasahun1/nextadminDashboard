c5ea32b2b47940e078acc1523c572fd55ac223d8
kasahun1 <kasuabebe12@gmail.com>
2024-06-19 21:46:15 +0300

    dashboard finished

--- app/dashboard/layout.jsx
+++ app/dashboard/layout.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import SideBar from "../ui/dashboard/sidebar/page";
 import styles from "../ui/dashboard/dashboard.module.css"
 import Navbar from "../ui/dashboard/navbar/page";
+import Footer from "../ui/dashboard/footer/footer";
 
 const Layout = ({children}) => {
   return (
@@ -12,6 +13,7 @@ const Layout = ({children}) => {
         <div className={styles.content}>
         <Navbar/>
         {children}
+        <Footer/>
         </div>
     </div>
   )
--- app/ui/dashboard/card/card.jsx
+++ app/ui/dashboard/card/card.jsx
@@ -6,13 +6,13 @@ const Card = ({item}) => {
     <div className={styles.container}>
     <MdSupervisedUserCircle size={24} />
     <div className={styles.texts}>
-      <span className={styles.title}>{item.title}</span>
-      <span className={styles.number}>{item.number}</span>
+      <span className={styles.title}>Total users</span>
+      <span className={styles.number}>11.23</span>
       <span className={styles.detail}>
-        <span className={item.change > 0 ? styles.positive : styles.negative}>
-          {item.change}%
+        <span className={ styles.positive }>
+          12%
         </span>{" "}
-        {item.change > 0 ? "more" : "less"} than previous week
+        more than previous week
       </span>
     </div>
   </div>
